b2fcd6c793b9df80248288d30ca76cf3fce2599a
cyrexin <cyrexin@gmail.com>
2016-04-09 15:57:53 -0400

    some refactorings

--- application.py
+++ application.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python2.7
 
 import os
-import click
 from sqlalchemy import *
 from flask import Flask, request, render_template, g, redirect, Response, session, jsonify
-from config import *
-from data_access.user_data_access import *
+from server.config import *
+from server.data_access.user_data_access import *
 
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates')
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 # set the secret key.  keep this really secret:
@@ -86,25 +85,27 @@ def register():
 
 if __name__ == "__main__":
 
-    @click.command()
-    @click.option('--debug', is_flag=True)
-    @click.option('--threaded', is_flag=True)
-    @click.argument('HOST', default='0.0.0.0')
-    @click.argument('PORT', default=8111, type=int)
-    def run(debug, threaded, host, port):
-        """
-            This function handles command line parameters.
-            Run the server using:
-            python server.py
-
-            Show the help text using:
-
-            python server.py --help
-
-        """
-
-        HOST, PORT = host, port
-        print "running on %s:%d" % (HOST, PORT)
-        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-
-    run()
+    # @click.command()
+    # @click.option('--debug', is_flag=True)
+    # @click.option('--threaded', is_flag=True)
+    # @click.argument('HOST', default='0.0.0.0')
+    # @click.argument('PORT', default=5000, type=int)
+    # def run(debug, threaded, host, port):
+    #     """
+    #         This function handles command line parameters.
+    #         Run the server using:
+    #         python server.py
+    #
+    #         Show the help text using:
+    #
+    #         python server.py --help
+    #
+    #     """
+    #
+    #     HOST, PORT = host, port
+    #     print "running on %s:%d" % (HOST, PORT)
+    #     app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+    #
+    # run()
+    app.debug = True
+    app.run()
