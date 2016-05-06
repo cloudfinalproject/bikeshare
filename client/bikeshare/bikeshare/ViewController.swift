945fe59ae3ab1e9ffedee998301ab46bc175fbcc
houlianglv <houlianglv@163.com>
2016-05-05 22:42:43 -0400

    add navigation tab and tab views

--- client/bikeshare/bikeshare/ViewController.swift
+++ client/bikeshare/bikeshare/ViewController.swift
@@ -11,15 +11,20 @@ import GoogleMaps
 
 var serverDomain = "http://localhost:5000"
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITabBarDelegate {
 
 
 
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var gmsMapView: GMSMapView!
-
+    @IBOutlet weak var tabBar: UITabBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //tabBar delegate
+        tabBar.delegate = self
+
         // Do any additional setup after loading the view, typically from a nib.
         let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
             longitude: 151.20, zoom: 6)
@@ -56,6 +61,24 @@ class ViewController: UIViewController {
 
         self.performSegueWithIdentifier("goto_login", sender: self)
     }
+
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        let index = Int((self.tabBar.items?.indexOf(item))!)
+        switch index {
+        case 0:
+            self.performSegueWithIdentifier("goto_dashboard", sender: self)
+        case 1:
+            self.performSegueWithIdentifier("goto_messages", sender: self)
+        case 2:
+            self.performSegueWithIdentifier("goto_bikes", sender: self)
+        case 3:
+            self.performSegueWithIdentifier("goto_profile", sender: self)
+        default:
+            return
+        }
+
+    }
+
 
 }
 
