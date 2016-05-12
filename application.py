0a57bf38462443f16d584cb0cbc9186566533645
cyrexin <cyrexin@gmail.com>
2016-05-12 11:48:35 -0400

    messages

--- application.py
+++ application.py
@@ -246,33 +246,32 @@ def update_profile():
 
 
 ######## Message ########
-@application.route('/view/sendMsg')
-def view_send_msg():
-    return render_template('sendMsg.html')
-
-@application.route('/view/showMsg')
-def view_show_msg():
-    return render_template('showMsg.html')
-
 @application.route('/sendMsg', methods=['POST'])
-def sendMsg():
-    uid1 = session['uid']
-    uid2 = request.form['toWhom']
-    message = request.form['message']
+def send_message():
+    if not session or 'uid' not in session:
+        return abort(403)
+    else:
+        uid = session['uid']
+        rid = request.form['rid']
+        # uid2 = request.form['toWhom']
+        message = request.form['message']
 
-    uma = UserMsgAccess(g.conn)
-    output = uma.sendMsg(uid1, uid2, message)
-
-    return jsonify(output)
-
-@application.route('/showMsg', methods=['REQUEST'])
-def showMsg():
-    uid = session['uid']
-
-    uma = UserMsgAccess(g.conn)
-    output = uma.showMsg(uid)
+        uma = UserMsgAccess(g.conn)
+        output = uma.send_message(uid, rid, message)
 
     return jsonify(output)
+
+@application.route('/showMessages/<rid>')
+def show_messages(rid):
+    if not session or 'uid' not in session:
+        return abort(403)
+    else:
+        uid = session['uid']
+
+        uma = UserMsgAccess(g.conn)
+        output = uma.show_messages(rid)
+
+        return jsonify(output)
 
 
 ######## Request ########
--- server/data_access/user_msg_access.py
+++ server/data_access/user_msg_access.py
@@ -1,21 +1,38 @@
+from user_data_access import *
+
+
 class UserMsgAccess:
     def __init__(self, conn):
         self.conn = conn
 
-    def showMsg(self, uid):
+    def show_messages(self, rid):
         output = {'result': {}, 'status': False, 'message': ''}
-        msg = {}
+        messages = []
         status = False
         message = ''
         try:
-            cursor = self.conn.execute("SELECT * FROM messages WHERE uid2 = %s", (uid, ))
+            cursor = self.conn.execute("SELECT m.*, r.uid FROM messages m, requests r WHERE m.rid = r.rid and m.rid = %s order by creationdate desc", (rid, ))
             for row in cursor:
-                cursor2 = self.conn.execute("SELECT username FROM users WHERE uid = %s", (row['uid1'], ))
-                for row2 in cursor2:
-                    msg[row['mid']] = {}
-                    msg[row['mid']]['from'] = row2['username']
-                    msg[row['mid']]['content'] = row['message']
-                    msg[row['mid']]['timestamp'] = row['creationdate']
+                msg = dict(row)
+
+                uda = UserDataAccess(self.conn)
+                owner_id = msg['uid1']
+                owner = uda.get_user(owner_id)
+                msg['owner'] = owner['result']['user']
+                requester_id = msg['uid']
+                requester = uda.get_user(requester_id)
+                msg['requester'] = requester['result']['user']
+
+                # remove unnecessary fields
+                msg.pop('uid', None)
+                msg.pop('uid1', None)
+                # msg.pop('uid2', None)
+
+                messages.append(msg)
+            cursor.close()
+
+            status = True
+            message = "All the messages for this request has been retrieved successfully."
              
         except Exception, e:
             status = False
@@ -23,34 +40,35 @@ class UserMsgAccess:
             raise e
 
         finally:
-            cursor.close()
             output['status'] = status
             output['message'] = message
-            output['result']['messages'] = msg
+            output['result'] = messages
             return output
-            
 
-    def sendMsg(self, uid1, uid2, msgToSend):
+    def send_message(self, uid1, rid, contents):
         output = {'result': {}, 'status': False, 'message': ''}
-        user = {}
         status = False
         message = ''
         try:
-            cursor = self.conn.execute("""insert into messages(uid1, uid2, message, creationdate)
-            values (%s, %s, %s, now()) returning mid, creationdate""", (uid1, uid2, msgToSend))
+            cursor = self.conn.execute("""insert into messages(uid1, message, creationdate, rid)
+            values (%s, %s, now(), %s) returning mid, creationdate""", (uid1, contents, rid))
             for row in cursor:
                 new_msg_id = row['mid']
                 creationdate = row['creationdate']
-            output['result']['mid'] = new_msg_id
-            output['result']['creationdate'] = creationdate
+                output['result']['mid'] = new_msg_id
+                output['result']['creationdate'] = creationdate
+            cursor.close()
+
             message = "Message sent successfully!"
+            status = True
         except Exception, e:
             status = False
             message = e
             raise e
 
-        finally:  
-            cursor.close()
+        finally:
+            output['status'] = status
+            output['message'] = message
             return output
 
 
